7f6dd6a67068fbeda53925ac695c3fa3be4a0cc1
Wei Wei <shazhouke@gmail.com>
2023-12-09 17:59:44 +0800

    Add PureScript type notation for Series functions

--- @node-frp/core/src/lib/Series.ts
+++ @node-frp/core/src/lib/Series.ts
@@ -2,15 +2,18 @@ import { Eff, Fn, Handler, Observe } from './Types';
 
 export type Series<A> = Fn<Handler<A>, void>;
 
+// map :: forall a b. a -> b -> (Series a) -> Series b
 export const mapSeries =
   <A, B>(f: Fn<A, B>) =>
   (ser: Series<A>): Series<B> =>
   (hdl) =>
     ser((a) => hdl(f(a)));
 
+// observe :: forall a. Series a -> (a -> Effect Unit) -> Effect (Effect Unit)
 export const observeSeries =
   <A>(ser: Series<A>): Observe<A> =>
-  (hdl) => () => {
+  (hdl) =>
+  () => {
     let hdlT: Handler<A> | null = (val) => () =>
       hdlT && (ser(hdlT), hdl(val)());
     ser(hdlT);
@@ -19,4 +22,9 @@ export const observeSeries =
     };
   };
 
-export const once = <A>(ser: Series<A>) => (hdl: Handler<A>): Eff => () => ser(hdl);
+// once :: forall a. Series a -> (a -> Effect Unit) -> Effect Unit
+export const once =
+  <A>(ser: Series<A>) =>
+  (hdl: Handler<A>): Eff =>
+  () =>
+    ser(hdl);
