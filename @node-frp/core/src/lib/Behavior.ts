6c2df93eec9d1c439c140e628e00a9cca79c69d8
Wei Wei <shazhouke@gmail.com>
2023-12-09 17:54:29 +0800

    Add PureScript type notation for Behavior functions

--- @node-frp/core/src/lib/Behavior.ts
+++ @node-frp/core/src/lib/Behavior.ts
@@ -22,26 +22,32 @@ export const behavior = <A>(fn: Fn<Eff, A>): Behavior<A> => {
   };
 };
 
+// pure :: forall a. a -> Behavior a
 export const pureBehavior =
   <A>(val: A): Behavior<A> =>
   () =>
     val;
 
+ 
+// map :: forall a b. (a -> b) -> Behavior a -> Behavior b
 export const mapBehavior =
   <A, B>(f: (t: A) => B) =>
   (beh: Behavior<A>): Behavior<B> =>
     behavior((eff) => f(beh(eff)));
 
+// apply :: forall a b. Behavior (a -> b) -> Behavior a -> Behavior b
 export const applyBehavior =
   <A, B>(behF: Behavior<Fn<A, B>>) =>
   (behA: Behavior<A>): Behavior<B> =>
     behavior((eff) => behF(eff)(behA(eff)));
 
+// bind :: forall a b. Behavior a -> (a -> Behavior b) -> Behavior b
 export const bindBehavior =
   <A>(beh: Behavior<A>) =>
   <B>(f: Fn<A, Behavior<B>>): Behavior<B> =>
     behavior((eff) => f(beh(eff))(eff));
 
+// observe :: forall a. Behavior a -> (a -> Effect Unit) -> Effect (Effect Unit)
 export const observeBehavior =
   <A>(beh: Behavior<A>): Observe<A> =>
   (hdl) =>
@@ -53,9 +59,13 @@ export const observeBehavior =
     };
   };
 
+// peek :: forall a. Behavior a -> Effect a
 export const peekBehavior =
   <A>(beh: Behavior<A>): Peek<A> => () => beh(null);
 
+// deflicker' :: forall a. (a -> a -> Bool) -> Behavior a -> Behavior a
+// This is for the constrained version
+// deflicker  :: forall a. Eq a => Behavior a -> Behavior a
 export const deflicker =
   <A>(eq: Fn<A, Fn<A, boolean>>) =>
   (beh: Behavior<A>): Behavior<A> =>
